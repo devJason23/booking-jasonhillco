95684b0601eab1bc8ba3f1b146f74e6c5be97d89
devJason23 <mdm.jason@gmail.com>
2025-04-27 20:22:38 -0500

    Update App.tsx
    change browserrouter

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/booking">  {/* ✅ Added basename */}
+      <BrowserRouter>  {/* ✅ Added basename */}
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
